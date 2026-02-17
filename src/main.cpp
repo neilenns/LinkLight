755c8f1b7e650a526bc877c170b59bf084a7e021
Neil Enns <neile@live.com>
2026-02-17 07:11:02 -0800

    Use WS2815 LED instead Fixes #26

--- src/main.cpp
+++ src/main.cpp
@@ -45,12 +45,13 @@ void loop() {
   // Update train positions periodically
   if (millis() - lastApiUpdate > API_UPDATE_INTERVAL) {
     trainDataManager.updateTrainPositions();
-    lastApiUpdate = millis();
-  }
-  
+    
   // Display current train positions on LEDs
   ledController.displayTrainPositions();
-  
+
+  lastApiUpdate = millis();
+  }
+    
   delay(10);
 }
 

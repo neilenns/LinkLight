bdc4be4c9b46efdb6b0e43896866626c989cdb6c
Neil Enns <neile@live.com>
2026-02-18 13:22:31 -0800

    Fix default colours

--- include/config.h
+++ include/config.h
@@ -43,8 +43,8 @@
 #define DEFAULT_HOSTNAME "LinkLight"
 #define DEFAULT_TIMEZONE "PST8PDT,M3.2.0,M11.1.0"  // Pacific Time with DST
 #define DEFAULT_UPDATE_INTERVAL 30  // Default update interval in seconds
-#define DEFAULT_LINE1_COLOR "#00ff00"  // Green for Line 1
-#define DEFAULT_LINE2_COLOR "#0000ff"  // Blue for Line 2
-#define DEFAULT_SHARED_COLOR "#ffff00"  // Yellow for shared/overlap
+#define DEFAULT_LINE1_COLOR "#002300"  // Green for Line 1
+#define DEFAULT_LINE2_COLOR "#000023"  // Blue for Line 2
+#define DEFAULT_SHARED_COLOR "#232300"  // Yellow for shared/overlap
 
 #endif // CONFIG_H
